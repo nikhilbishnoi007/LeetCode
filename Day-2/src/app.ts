3fb29ee6979ea6df15cf74efef75b23ffebbad95
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-08-30 15:45:54 +0530

    solve lenght of last word  question

--- Day-2/src/app.ts
+++ Day-2/src/app.ts
@@ -1,7 +1,20 @@
-function strStr(haystack: string, needle: string): number {
-let index=haystack.indexOf(needle)
-return index
+// Q1)  Find the Index of the First Occurrence in a String
+// function strStr(haystack: string, needle: string): number {
+// let index=haystack.indexOf(needle)
+// return index
+// };
+
+// console.log(strStr("sadbutsad", "sad"))
+// console.log(strStr("leetcode", "leeto"))
+
+//Q2)Length of Last Word
+
+
+function lengthOfLastWord(s: string): number {
+    let lastWord: string = s.trim().split(" ").at(-1)!;
+    let lengthOfLastWord=lastWord.length
+    return lengthOfLastWord
 };
 
-console.log(strStr("sadbutsad", "sad"))
-console.log(strStr("leetcode", "leeto"))
+console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord("   fly me  to   the moon  "))
