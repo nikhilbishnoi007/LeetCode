af06c338bb812c7dc181ac0ea671fe1b2b304c17
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-09-10 15:53:59 +0530

    leetcode problem:53 maxium subbary sum

--- Array/src/app.ts
+++ Array/src/app.ts
@@ -19,7 +19,7 @@
 // let find=secondLargestNumber([12, 35, 38, 36, 34, 1])
 // console.log(find)
 
-//Q.2 chnge the index of 0
+//Q.2 change the index of 0
 // function moveZeros(arr: number[]): number[] {
 //  let insertPos = 0
 //  for(const val of arr){
@@ -48,21 +48,45 @@
 //     return missingnumber
 // }
 
-// console.log(findMissing([1, 2, 4, 5], 6)); 
+// console.log(findMissing([1, 2, 4, 5], 5)); 
 
 //Q.4 finding multiple missing number
 
-function multipleMissingNumber(arr: number[],n:number):number[]{
-    let set=new Set<number>()
-    for(let i=1;i<=n;i++){
-        set.add(i)
+// function multipleMissingNumber(arr: number[],n:number):number[]{
+//     let set=new Set<number>()
+//     for(let i=1;i<=n;i++){
+//         set.add(i)
+//     }
+//     for(const val of arr){
+//         set.delete(val)
+//     }
+//      return Array.from(set);
+// }
+
+// let array=[1,2,4,5,7]
+// let value=multipleMissingNumber(array,9)
+// console.log(value)
+
+
+//Q.5 find maxium subbarrays sum
+function maxSubarraySum(arr: number[]): number {
+  let currentSum = arr[0]; 
+  let maxSum = arr[0]; 
+
+  for (let i = 1; i < arr.length; i++) {
+    currentSum = Math.max(arr[i], currentSum + arr[i])
+    if(currentSum>maxSum){
+        maxSum=currentSum
     }
-    for(const val of arr){
-        set.delete(val)
-    }
-     return Array.from(set);
+  }
+
+  return maxSum;
 }
 
-let array=[1,2,4,5,7]
-let value=multipleMissingNumber(array,9)
-console.log(value)
+
+
+console.log(maxSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4])); 
+
+
+
+// output: 6 (subarray [4, -1, 2, 1] ka sum sabse zyada hai)
